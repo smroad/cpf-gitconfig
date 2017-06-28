6b9188dc0893f9e2bcaefe62b9728a1abacd63f6
wangjialin <wangjialin@foxmail.com>
2017-06-28 11:04:36 +0800

    first commit

---
+++ src/main/java/org/jlhh/cpf/gitconfig/GitconfigApplication.java
@@ -0,0 +1,11 @@
+package org.jlhh.cpf.gitconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitconfigApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GitconfigApplication.class, args);
+	}
+}
